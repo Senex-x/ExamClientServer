85dcbc2c95faecfbf5e6eed2ca3a34a5b5928dfb
V-Senex <vdm.snx@gmail.com>
2020-10-24 17:19:56 +0300

    Receiving and parsing json fixed

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,12 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.json.simple.JSONObject;
 
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Client {
@@ -18,8 +22,8 @@ public class Client {
     static private final String OUT2 = "Multiply";
 
     static private Socket socket;
-    static private DataInputStream inputStream;
-    static private DataOutputStream outputStream;
+    static private InputStream inputStream;
+    static private OutputStream outputStream;
 
     static boolean end;
 
@@ -34,22 +38,20 @@ public class Client {
             outputStream.write(startRequest.toString().getBytes());
 
             // receiving and displaying json response file
-            displayInput(null);
 
-/*
-            BufferedReader data = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuilder content = new StringBuilder();
-            int value;
-            while ((value = data.read()) != -1) {
-                System.out.print((char)value);
-                content.append((char) value);
-                log(-1);
-            }
-            log(0);
-            System.out.println(content);
-*/
+            byte[] b = new byte[64];
+            Gson gson = new Gson();
+            inputStream.read(b);
+            String responseS = new String(b);
+            responseS = responseS.replaceAll("\\p{C}", "");
+            System.out.println(responseS);
+            ResponseItem responseItem = gson.fromJson(responseS, ResponseItem.class);
+            System.out.println(responseItem.toString());
+
+
+
             // sending student's method's outputs to server
-            outputStream.write(startRequest.toString().getBytes());
+
 
             socket.close();
         } catch (IOException e) {
@@ -61,28 +63,32 @@ public class Client {
     private static void init() throws IOException {
         socket = new Socket();
         socket.connect(new InetSocketAddress(HOSTNAME_REMOTE, PORT_REMOTE), 2000);
-        inputStream = new DataInputStream(socket.getInputStream());
-        outputStream = new DataOutputStream(socket.getOutputStream());
+        inputStream = socket.getInputStream();
+        outputStream = socket.getOutputStream();
     }
 
     private static JSONObject createStartRequestJson() {
         JSONObject startRequest = new JSONObject();
         startRequest.put(TYPE, 0);
-        startRequest.put(OUT1, -2);
-        startRequest.put(OUT2, -2);
+        startRequest.put(OUT1, -1);
+        startRequest.put(OUT2, -1);
         return startRequest;
     }
 
     private static ResponseItem getServerResponse() throws IOException {
-        BufferedReader data = new BufferedReader(new InputStreamReader(inputStream));
+        ResponseItem responseItem = new ResponseItem();
+        BufferedReader inputData = new BufferedReader(new InputStreamReader(inputStream));
         StringBuilder content = new StringBuilder();
         int value;
-        while ((value = data.read()) != -1) {
+        while ((value = inputData.read()) != -1) {
             content.append((char) value);
+            if ((char) value == ':') {
+            }
+            if ((char) value == '}') break;
         }
-        data.close();
+        inputData.close();
         System.out.println(content);
-        return new ResponseItem();
+        return responseItem;
     }
 
     private static void displayInput(BufferedReader in) throws IOException {
--- src/ResponseItem.java
+++ src/ResponseItem.java
@@ -1,20 +1,44 @@
 public class ResponseItem {
-    private static int X;
-    private static int Y;
+    private String Type;
+    private int X;
+    private int Y;
 
-    public static int getX() {
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public ResponseItem() {
+
+    }
+
+    public ResponseItem(String type, int x, int y) {
+        Type = type;
+        X = x;
+        Y = y;
+    }
+
+    public int getX() {
         return X;
     }
 
-    public static void setX(int x) {
+    public void setX(int x) {
         X = x;
     }
 
-    public static int getY() {
+    public int getY() {
         return Y;
     }
 
-    public static void setY(int y) {
+    public void setY(int y) {
         Y = y;
+    }
+
+    @Override
+    public String toString() {
+        return  Type + " " + X + " " + Y;
     }
 }
