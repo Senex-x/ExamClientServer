c4306a724843998f4ab082cb8d8ae9512a276fbd
V-Senex <vdm.snx@gmail.com>
2020-10-27 11:09:23 +0300

    Fatal errors with requests fixed

--- src/Client.java
+++ src/Client.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONObject;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
 
 class Client {
     private static final String HOSTNAME_LOCAL = "127.0.0.1";
@@ -11,11 +12,11 @@ class Client {
     private static final String HOSTNAME_REMOTE = "157.245.129.238";
     private static final int PORT_REMOTE = 11000;
 
-    private static final String NAME = "name";
+    private static final String NAME = "Name";
     private static final String TYPE = "MessageType";
-    private static final String OUT1 = "normalized";
-    private static final String OUT2 = "proximities";
-    private static final String OUT3 = "resultClass";
+    private static final String OUT1 = "Normalized";
+    private static final String OUT2 = "Proximities";
+    private static final String OUT3 = "ResultClass";
 
     private static final String RESULT_AC = "\nVERDICT: ACCEPTED";
     private static final String RESULT_WA = "\nVERDICT: WRONG ANSWER";
@@ -62,6 +63,23 @@ class Client {
                     // runtime error
                     System.out.println(RESULT_RE);
                     break;
+                case 4:
+                    // accepted
+                    System.out.println(RESULT_AC);
+                    break;
+                case 5:
+                    // accepted
+                    System.out.println(RESULT_AC);
+                    break;
+                case 6:
+                    // accepted
+                    System.out.println(RESULT_AC);
+                    break;
+                case 7:
+                    // accepted
+                    System.out.println(RESULT_AC);
+                    break;
+
             }
 
             socket.close();
@@ -72,14 +90,14 @@ class Client {
 
     private static void sendStartRequest() throws IOException {
         // making startRequest json file
-        JSONObject startRequest = createJsonResponse(0, new double[][] {new double[] {0}}, new double[] {0}, -1);
+        JSONObject startRequest = createJsonResponse(0, new double[][] {new double[] {0}}, new double[] {0}, 0);
         display("Sent request message: ", startRequest.toString());
         // sending startRequest json file to server
         outputStream.write(startRequest.toString().getBytes());
     }
 
     private static ResponseItem getServerResponse() throws IOException {
-        byte[] byteData = new byte[64];
+        byte[] byteData = new byte[4096];
         // receiving json file and writing it to byte array
         inputStream.read(byteData);
         // making string from received byte array
@@ -100,11 +118,14 @@ class Client {
     }
 
     private static JSONObject createJsonResponse(int type, double[][] first, double[] second, int third) {
-        System.out.println("_____" + second[0]);
+        Gson gson = new Gson();
         JSONObject jsonObject = new JSONObject();
+        jsonObject.put(NAME, "Sample");
         jsonObject.put(TYPE, type);
-        jsonObject.put(OUT1, first);
-        jsonObject.put(OUT2, getString(second));
+        //jsonObject.put(OUT1, gson.toJson(first));
+        //jsonObject.put(OUT2, gson.toJson(second));
+        jsonObject.put(OUT1, null);
+        jsonObject.put(OUT2, null);
         jsonObject.put(OUT3, third);
         return jsonObject;
     }
